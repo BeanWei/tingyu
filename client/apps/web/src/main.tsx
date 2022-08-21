0b46f11c6e3a41cb164c0d2f45025067d16e308e
Bean.Wei <157318439@qq.com>
2022-08-21 22:52:16 +0800

    🎉 init(client/web): init client/web

---
+++ client/apps/web/src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
+import { BrowserRouter } from 'react-router-dom';
+import 'uno.css';
+import ErrorFallback from '~sections/ErrorFallback';
+import '~styles/styles.less';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
+  </React.StrictMode>,
+);
