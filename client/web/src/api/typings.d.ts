065dfe8064edfd25d332032b302859d6deecf455
Bean.Wei <157318439@qq.com>
2022-08-26 21:43:59 +0800

    🐞 fix(web): api typing

--- client/web/src/api/typings.d.ts
+++ client/web/src/api/typings.d.ts
@@ -1,5 +1,12 @@
 declare namespace API {
+  type UserLoginReq = {
+    username: string
+    password: string
+  }
   type UserLoginResp = {
     token: string
+  }
+  type GetUserInfoReq = {
+    id?: string
   }
 }
--- client/web/src/api/user.ts
+++ client/web/src/api/user.ts
@@ -1,14 +1,14 @@
 import type { Promise } from './base'
 import { api } from './base'
 
-export const userLogin = (data: any): Promise<API.UserLoginResp> => {
+export const userLogin = (data: API.UserLoginReq): Promise<API.UserLoginResp> => {
   return api.execute('/v1/user/login', {
     method: 'post',
     data,
   })
 }
 
-export const getUserInfo = (params: any) => {
+export const getUserInfo = (params: API.GetUserInfoReq) => {
   return api.execute('/v1/user/get', {
     method: 'get',
     params,
